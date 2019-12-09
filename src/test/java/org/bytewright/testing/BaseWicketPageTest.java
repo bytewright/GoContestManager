799e3a48330854b8450e369624d17b689de5ed3c
Elias Lerch <elerch@novomind.com>
2019-12-09 13:28:59 +0100

    Fix: Make wicket testing with junit and spring possible

--- src/test/java/org/bytewright/testing/BaseWicketPageTest.java
+++ src/test/java/org/bytewright/testing/BaseWicketPageTest.java
@@ -14,7 +14,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  */
 @ContextConfiguration(classes = SpringTestContext.class)
 @RunWith(SpringJUnit4ClassRunner.class)
-public class BaseWicketPageTest {
+public abstract class BaseWicketPageTest {
 
   @Autowired
   protected WicketApplication wicketApplication;
