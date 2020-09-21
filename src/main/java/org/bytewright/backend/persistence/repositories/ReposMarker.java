17514bd19028ed1c283bdb861eefbc0e71cb6446
Elias Lerch <elerch@novomind.com>
2020-09-21 17:01:19 +0200

    Dep updates, maven plugin for dep checks and readme

---
+++ src/main/java/org/bytewright/backend/persistence/repositories/ReposMarker.java
@@ -0,0 +1,4 @@
+package org.bytewright.backend.persistence.repositories;
+
+public class ReposMarker {
+}
--- src/test/java/org/bytewright/TestHomePage.java
+++ src/test/java/org/bytewright/TestHomePage.java
@@ -1,6 +1,5 @@
 package org.bytewright;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
 import org.bytewright.frontend.pages.HomePage;
 import org.bytewright.testing.BaseWicketPageTest;
 import org.junit.Test;
